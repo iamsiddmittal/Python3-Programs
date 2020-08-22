3ac1363f3bf7ca9fd1955a080f7e1cc601d5b545
Sidd Mittal <66897619+iamSiddMittal@users.noreply.github.com>
2020-08-22 10:06:27 +0530

    Add comments

--- MakeACopy.py
+++ MakeACopy.py
@@ -2,11 +2,15 @@ original_file_name = input("Enter the name/path of the file you want to make a c
 
 original_file = open(original_file_name)
 
-
+# Setting the file name of the copy file
 copy_file_name = original_file_name.split(".")
 copy_file_name = copy_file_name[0] + "-COPY." + copy_file_name[1]
 
+# Writing to the copy file
 copy_file = open(copy_file_name,"x")
 copy_file.write(original_file.read())
 
+
+original_file.close()
+copy_file.close()
 print("\nTask successful!")
