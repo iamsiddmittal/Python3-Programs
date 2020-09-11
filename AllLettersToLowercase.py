47ff8323a6f742d7e56cd7a45cce11e57e895096
Sidd Mittal <66897619+iamSiddMittal@users.noreply.github.com>
2020-09-11 21:59:59 +0530

    Add support for text files

--- AllLettersToLowercase.py
+++ AllLettersToLowercase.py
@@ -1,3 +1,14 @@
-i = input("Enter word/phrase: ")
+# Works for file/string/word/phrase
 
-print( i.lower() )
+i = input("Enter word/phrase/filename: ")
+
+try:
+    f_obj = open(i, "r+")
+    new_content = f_obj.read().lower()
+
+    f_obj.seek(0)
+    f_obj.write( new_content )
+
+    print("Content inside the file has been converted into lowercase")
+except:
+    print( i.lower() )
--- AllLettersToUppercase.py
+++ AllLettersToUppercase.py
@@ -1,3 +1,14 @@
-i = input("Enter word/phrase: ")
+# Works for file/string/word/phrase
 
-print( i.upper() )
+i = input("Enter word/phrase/filename: ")
+
+try:
+    f_obj = open(i, "r+")
+    new_content = f_obj.read().upper()
+
+    f_obj.seek(0)
+    f_obj.write( new_content )
+
+    print("Content inside the file has been converted into uppercase")
+except:
+    print( i.upper() )
