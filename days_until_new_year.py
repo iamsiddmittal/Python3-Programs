2ed0119922b2e0e9ba5a3e10825e0429a3b75e8a
Sidd Mittal <66897619+iamSiddMittal@users.noreply.github.com>
2020-09-19 09:50:25 +0530

    Add Days Left And Exact Time

--- days_until_new_year.py
+++ days_until_new_year.py
@@ -1,10 +1,14 @@
-### Days till new year
-
 from datetime import datetime
 
 now = datetime.now()
 new_year = now.year + 1
 
-days_left = (datetime(new_year,1,1) - now) #datetime(year,month,day)
+# timedelta object
+days_left = datetime(new_year,1,1) - datetime(now.year, now.month, now.day)
 
-print(days_left,"remaining till the next year",sep="\n")
+# datetime object
+exact_time = (datetime(new_year,1,1) - now) #datetime(year,month,day)
+
+print()
+print(f"Days left till new year: {days_left.days}", end="\n\n")
+print(f"Exact time: {exact_time}", end="\n\n")
