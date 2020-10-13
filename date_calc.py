0d04cfaa36300f31f67d077f44a232830b1a02d1
Sidd Mittal <66897619+iamSiddMittal@users.noreply.github.com>
2020-10-13 23:48:10 +0530

    Add Difference Of Dates Operation

--- date_calc.py
+++ date_calc.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 operation = input("Enter the operation you want to perform (add)\n>>> ")
 
+
 if operation in ["add", "Add", "ADD"]:
     add_to_date = list(map(int, input("Enter Date (Year Month Day): ").strip().split(" ")))
     add_to_date = datetime(add_to_date[0], add_to_date[1], add_to_date[2])
@@ -11,6 +12,20 @@ if operation in ["add", "Add", "ADD"]:
 
     result = add_to_date + timedelta(add)
     print(result)
+
+
+elif operation in ["sub", "Sub", "SUB", "diff", "Diff", "DIFF"]:
+    sub_from_date = list(map(int, input("Enter Date (Year Month Day) To Subtract From: ") \
+                                        .strip().split(" ")))
+    sub_from_date = datetime(sub_from_date[0], sub_from_date[1], sub_from_date[2])
+
+    sub = list(map(int, input("Enter Date (Year Month Day) To Subtract: ") \
+                            .strip().split(" ")))
+    sub = datetime(sub[0], sub[1], sub[2])
+
+    result = sub_from_date - sub
+    print(result)
+
 
 else:
     print("Not a valid operation code!!")
